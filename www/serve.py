db6fd387558c571caaaff8578d62a1f59cbde2a9
Arjun Satarkar <me@arjunsatarkar.net>
2023-02-15 12:12:25 +0000

    Vastly improve README.adoc, improve www structure

--- www/serve.py
+++ www/serve.py
@@ -3,28 +3,45 @@ import os
 import pathlib
 import sys
 
-PORT = 80
+PORT = 27701
 if len(sys.argv) > 1:
-	PORT = int(sys.argv[1])
+    PORT = int(sys.argv[1])
 
 parent = pathlib.Path(__file__).parent.resolve()
-webroot = parent/"webroot"
+webroot = parent / "webroot"
+
 
 @bottle.route("/")
 def serve_index():
-	return bottle.static_file("index.html", root=webroot)
+    return bottle.static_file("index.html", root=webroot / "content")
+
 
 @bottle.route("/index")
 def reject_noncanonical_index():
-	return bottle.HTTPError(404)
+    return bottle.HTTPError(404)
 
-@bottle.route("/<path:path>")
-def serve(path):
-	if os.path.isfile(webroot / (path + ".html")):
-		return bottle.static_file(path + ".html", root=webroot)
-	else:
-		if path.endswith(".html"):
-			return bottle.HTTPError(404)
-		return bottle.static_file(path, root=webroot)
+
+@bottle.route("/styles/<style>")
+def serve_styles(style):
+    return bottle.static_file(style, root=webroot / "styles")
+
+
+@bottle.route("/media/<media>")
+def serve_media(media):
+    return bottle.static_file(media, root=webroot / "media")
+
+
+@bottle.route("/<content>")
+def serve(content):
+    if os.path.isfile(webroot / "content" / (content + ".html")):
+        return bottle.static_file(content + ".html", root=webroot / "content")
+    else:
+        return bottle.static_file(content, root=webroot / "content")
+
+
+@bottle.error(404)
+def serve_404(error):
+    return bottle.static_file("404.html", root=parent)
+
 
 bottle.run(host="0.0.0.0", port=PORT)
