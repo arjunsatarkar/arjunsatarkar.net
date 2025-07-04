9836199d0d446700b5c27b00dc1fcb3c60fbc9d8
Arjun Satarkar <me@arjunsatarkar.net>
2025-07-04 17:05:02 +0530

    Preload fonts differently again, change font files
    Red herring on this method being too broad - works fine; the fonts are
    referenced in index.css so will be loaded on all pages that use it.
    
    The old font files from the Ubutu site were throwing errors in the
    console for some reason; these are from Google Fonts and seem better.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,10 @@
 <script>
     let { children } = $props();
     import "$lib/index.css";
-    import UbuntuR from "$lib/assets/ubuntu-font-family-0.83/Ubuntu-R.ttf";
-    import UbuntuM from "$lib/assets/ubuntu-font-family-0.83/Ubuntu-M.ttf";
-    import UbuntuMonoR from "$lib/assets/ubuntu-font-family-0.83/UbuntuMono-R.ttf";
-    import FontPreload from "./FontPreload.svelte";
     import { favicon } from "$lib";
 </script>
 
 <svelte:head>
-    <FontPreload fontPath={UbuntuR} fontType="font/ttf"></FontPreload>
-    <FontPreload fontPath={UbuntuM} fontType="font/ttf"></FontPreload>
-    <FontPreload fontPath={UbuntuMonoR} fontType="font/ttf"></FontPreload>
     <link rel="icon" href={favicon} />
 </svelte:head>
 
