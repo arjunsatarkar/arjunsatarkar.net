46ea108c5dc85c83b0145a155341dd77f98a7a1f
Arjun Satarkar <me@arjunsatarkar.net>
2025-07-04 10:22:24 +0530

    Change font preload strategy
    See arjunsatarkar/practicekana@17b3e52771c4f2de403b63dba9fd2868d306bc61

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
     let { children } = $props();
     import "$lib/index.css";
+    import UbuntuR from "$lib/assets/ubuntu-font-family-0.83/Ubuntu-R.ttf";
+    import UbuntuMonoR from "$lib/assets/ubuntu-font-family-0.83/UbuntuMono-R.ttf";
+    import FontPreload from "./FontPreload.svelte";
 </script>
+
+<svelte:head>
+    <FontPreload fontPath={UbuntuR} fontType="font/ttf"></FontPreload>
+    <FontPreload fontPath={UbuntuMonoR} fontType="font/ttf"></FontPreload>
+</svelte:head>
 
 {@render children()}
---
+++ src/routes/FontPreload.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let { fontPath, fontType }: { fontPath: string; fontType: string } =
+        $props();
+</script>
+
+<link
+    rel="preload"
+    href={fontPath}
+    as="font"
+    type={fontType}
+    crossorigin="anonymous"
+/>
