16d2241bc0fc5c998bf38ac4da563c0ddb119d11
Arjun Satarkar <me@arjunsatarkar.net>
2025-07-08 10:31:25 +0530

    Use @arjunsatarkar/sveltekit-canonicals

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { canonicals } from "@arjunsatarkar/sveltekit-canonicals"
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 import { sveltekit } from '@sveltejs/kit/vite';
@@ -16,16 +17,12 @@ export default defineConfig({
 				return code.replace('__FILE_SOURCE__', `'${SOURCE_REPO}/tree/main/${path.relative(__dirname, id)}'`);
 			}
 		},
-		{
-			name: "canonicals",
-			transform(code, id) {
-				const PAGE_SVELTE = "+page.svelte";
-				if (!id.endsWith("/" + PAGE_SVELTE)) return null;
-				let canonical = SITE_HOME + path.relative(path.resolve(__dirname, "src/routes"), id.slice(0, id.length - PAGE_SVELTE.length));
-				if (!canonical.endsWith("/")) canonical += "/";
-				return code.replace("__CANONICAL_URL__", `'${canonical}'`);
-			}
-		}
+		canonicals({
+			replace: "__CANONICAL_URL__",
+			routesDir: path.resolve(__dirname, "src/routes"),
+			siteUrlRoot: SITE_HOME,
+			trailingSlash: "always"
+		}),
 	],
 	define: {
 		__SITE_HOME__: JSON.stringify(SITE_HOME)
